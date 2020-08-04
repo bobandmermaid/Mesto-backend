ccff8db16e36f7aa922f2eb7c41e8659b5f37c56
Ilya Podolyak <bobandmermaid@yandex.com>
2020-08-04 23:09:55 +0300

    initial REST API, add MongoDB...

---
+++ controllers/users.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.getUserId = (req, res) => {
+  const userId = req.user._id;
+
+  User.findById(userId)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(404).send({ message: err.message }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(400).send({ message: err.message }));
+};
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  const owner = req.user._id;
+
+  User.findByIdAndUpdate(owner, { name, about })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(400).send({ message: err.message }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const owner = req.user._id;
+
+  User.findByIdAndUpdate(owner, { avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(400).send({ message: err.message }));
+};
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,12 @@
-const path = require('path');
+const router = require('express').Router();
+const {
+  getUsers, getUserId, createUser, updateUser, updateAvatar,
+} = require('../controllers/users');
 
-// eslint-disable-next-line import/no-dynamic-require
-const users = require(path.join(__dirname, '../data/users.json'));
+router.get('/', getUsers);
+router.get('/:userId', getUserId);
+router.post('/', createUser);
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar);
 
-const usersRouter = (req, res) => {
-  res.send(users);
-};
-
-const userRouter = (req, res) => {
-  const { id } = req.params;
-  const result = users.find(({ _id }) => _id === id);
-
-  if (!result) {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-    return;
-  }
-  res.send(result);
-};
-
-module.exports = {
-  usersRouter,
-  userRouter,
-};
+module.exports = router;
