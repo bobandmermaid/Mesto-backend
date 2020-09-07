4c8cec11262b9997315e0537e6340f471e3ae393
Ilya Podolyak <bobandmermaid@yandex.com>
2020-09-07 21:45:09 +0300

    del crash-test

--- app.js
+++ app.js
@@ -47,12 +47,6 @@ app.use(helmet());
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use('/signup', signup);
 app.use('/signin', signin);
 
